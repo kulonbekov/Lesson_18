8ae02453e4478902008875e83913d9eee2dc31df
Taalay <kulonbekov@gmail.com>
2022-09-01 18:44:18 +0600

    add enum Elements

--- src/kg/company/models/Elements.java
+++ src/kg/company/models/Elements.java
@@ -1,5 +1,21 @@
 package kg.company.models;
 
 public enum Elements {
+        Au("Gold", 2);
 
+        private String name;
+        private int number;
+
+    Elements(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
