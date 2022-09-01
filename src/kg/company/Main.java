4d04db9e9a47004c827b936128d5273e39e38763
Taalay <kulonbekov@gmail.com>
2022-09-01 18:48:26 +0600

    add main

--- src/kg/company/Main.java
+++ src/kg/company/Main.java
@@ -1,7 +1,10 @@
 package kg.company;
 
+import kg.company.models.Elements;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (Elements s: Elements.values())
+            System.out.println("Названия: " + s.getName() + ", атомный номер:" + s.getNumber());
     }
 }
