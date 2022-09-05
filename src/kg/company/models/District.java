134ecf337a57769aab244271919f1cfe337630a5
Taalay <kulonbekov@gmail.com>
2022-09-05 11:02:11 +0600

    add element in District (Naryn) branch master

--- src/kg/company/models/District.java
+++ src/kg/company/models/District.java
@@ -2,7 +2,8 @@ package kg.company.models;
 
 public enum District {
         Ik("Issyk-Kul", 9),
-        Ta("Talas",5);
+        Ta("Talas",5),
+        Na("Naryn", 5);
 
     private String name;
     private int regionNum;
